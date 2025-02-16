4bcc86e1f71fd2200c8cc1259e5b3bd7df2a9d81
Clement Cabus <chummycabus@gmail.com>
2025-02-16 10:44:48 +0800

    Cleaned Up some code -cleaned user.php and fortifyserviceprovider.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $table = 'admin_accounts'; // ✅ Ensure it's 'admin_accounts'
+    protected $table = 'admin_accounts';
     protected $primaryKey = 'id';
-    public $timestamps = false; // ✅ Disable timestamps if not in the table
+    public $timestamps = false;
 
     protected $fillable = [
         'name', 'username', 'password', 'profile_picture',
