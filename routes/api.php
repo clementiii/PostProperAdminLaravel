67e24fef6a86aaa781d2cb783e82a3d73c9e6322
Clement Cabus <chummycabus@gmail.com>
2025-04-20 15:35:15 +0800

    Chat fix ?

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ChatController;
+use App\Http\Controllers\API\ChatController;
 
 // Android Login 
 Route::post('android/login', function() {
